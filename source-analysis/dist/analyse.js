322db1fd45e6fdb4de69d3f1f5fbe87c19b54d0c
vagrant <vagrant@vagrant.vm>
2016-09-25 12:41:50 +0000

    1

--- source-analysis/dist/analyse.js
+++ source-analysis/dist/analyse.js
@@ -6,14 +6,16 @@ var Promise = require('promise');
 var config = require('./../analyse.json');
 var PCRE, ucg;
 var promises = [];
-var ucgOptions = config.ucg_options_line || process.argv[2] || '';
+var ucgOptions = config.ucg_options_line || process.argv[2];
 config.signatures.forEach(function (signature) {
     PCRE = ucg_wrapper_1.wrap('./../signatures/' + signature).replace(/\|\(\s*\)/g, '');
     if (/^win/.test(process.platform)) {
         ucg = child_process_1.spawn('cmd.exe', ['/c', ucgOptions, PCRE, config.target]);
     }
     else {
-        ucg = child_process_1.spawn('ucg', [ucgOptions, PCRE, config.target]);
+        ucg = child_process_1.spawn('ucg', [ PCRE, config.target]);
+console.log(ucg.spawnargs );
+
     }
     promises.push(new Promise(function (resolve) {
         ucg.stdout.on('data', function (data) {
